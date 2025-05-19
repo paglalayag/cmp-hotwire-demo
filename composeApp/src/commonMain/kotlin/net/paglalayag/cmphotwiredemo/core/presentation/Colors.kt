0299c45dc3e84e87fa5af01b070d75507235efce
graial <al@paglalayag.net>
2025-05-19 22:02:41 +0800

    add audio player icons

---
+++ composeApp/src/commonMain/kotlin/net/paglalayag/cmphotwiredemo/core/presentation/Colors.kt
@@ -0,0 +1,8 @@
+package net.paglalayag.cmphotwiredemo.core.presentation
+
+import androidx.compose.ui.graphics.Color
+
+val DarkRed = Color(0xff8e0f18)
+val LightGrey = Color(0xffebebeb)
+val Beige = Color(0xffd9c2a0)
+val LightRed = Color(0xffd56f6b)
